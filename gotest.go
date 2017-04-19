0a0ef62b44a03b43fff748f71503c8e61dbd878f
ochiba0227 <newgroundofswim@gmail.com>
2017-04-19 22:30:41 +0900

    func main()を含んではいけないらしい

--- gotest.go
+++ gotest.go
@@ -1,14 +1,18 @@
-package main
+package wajutsu
 
 import (
     "net/http"
     "github.com/labstack/echo"
 )
 
-func main() {
+func initEcho() {
     e := echo.New()
     e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "Hello, World!")
     })
     e.Logger.Fatal(e.Start(":8080"))
+}
+
+func init() {
+  initEcho()
 }
